0d7fe2af5c6b31c50cd8119acd51f7ec9e35e9fd
Val Dzhadan <dzhadan.valentyna.w@gmail.com>
2023-02-03 22:20:52 +0200

    changing basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/goit-react-hw05-movies">
     {/* <React.StrictMode> */}
     <App />
     {/* </React.StrictMode> */}
