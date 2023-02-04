617d4d8c0432316cfee09e1c72924391b4347f98
Val Dzhadan <dzhadan.valentyna.w@gmail.com>
2023-02-04 12:24:12 +0200

    adding eslint disable

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -19,6 +19,7 @@ export const useFetchData = (callback, deps = []) => {
       }
     };
     fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, deps);
   return {
     data,
